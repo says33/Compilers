0d7071529b53e2b12ed8bdbd8eb06341da40748b
Gamaliel Jimenez García <egjimenezg@gmail.com>
2014-02-26 11:50:02 -0600

    Metodo para comprobar que no hay estados sin marcar

---
+++ AnalizadorLexico/src/mx/ipn/analizadorLexico/domain/AFD.java
@@ -0,0 +1,58 @@
+package mx.ipn.analizadorLexico.domain;
+
+import java.util.ArrayList;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Gamaliel
+ * Date: 25/02/14
+ * Time: 05:39 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AFD {
+    private Estado edoInicial;
+    private ArrayList<Estado> estados;
+    private ArrayList<Integer> matriz;
+
+    public AFD(){};
+
+    public Estado getEdoInicial() {
+        return edoInicial;
+    }
+
+    public void setEdoInicial(Estado edoInicial) {
+        this.edoInicial = edoInicial;
+    }
+
+    public ArrayList<Estado> getEstados() {
+        return estados;
+    }
+
+    public void setEstados(ArrayList<Estado> estados) {
+        this.estados = estados;
+    }
+
+    public ArrayList<Integer> getMatriz() {
+        return matriz;
+    }
+
+    public void setMatriz(ArrayList<Integer> matriz) {
+        this.matriz = matriz;
+    }
+
+    /*Método utilizado para comprobar si hay estados sin marcar en el AFD*/
+    public boolean edosWithoutBeMarked(){
+        ArrayList<Estado> estados = new ArrayList<Estado>();
+        estados.add(this.edoInicial);
+
+        for(Estado e: this.estados)
+            estados.add(e);
+
+        for(int i=0;i<estados.size();i++){
+            if(estados.get(i).getId() == -1)
+                return true;
+        }
+
+        return false;
+    }
+}
---
+++ AnalizadorLexico/src/mx/ipn/analizadorLexico/utils/AFNaAFD.java
@@ -0,0 +1,24 @@
+package mx.ipn.analizadorLexico.utils;
+
+import mx.ipn.analizadorLexico.domain.AFD;
+import mx.ipn.analizadorLexico.domain.AFN;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Gamaliel
+ * Date: 26/02/14
+ * Time: 11:44 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AFNaAFD {
+
+    public AFNaAFD(){
+
+    }
+
+    public void convierteAFNaAFD(AFN afn,AFD afd){
+        while(afd.edosWithoutBeMarked()){
+
+        }
+    }
+}
